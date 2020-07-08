60d4788cb8221d995fef9d23ad9d19cfbac0cc23
freddie0621 <freddiebay@gmail.com>
2020-07-08 11:58:13 +0200

    Add files via upload

---
+++ mp3toimage.bat
@@ -0,0 +1,5 @@
+::http://pikooli.com/txttobutton/bouton.php?text=fsdflksdlmfksmdq%20sdfsdq
+
+
+for %%1 in ("*.mp3") do  wget --output-document=%%~n1.jpg http://pikooli.com/txttobutton/boutonjpg.php?text=%%~n1
+pause
